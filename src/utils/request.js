c10d8083150e2a4b19ed1c9e58fedec23905b0fd
chengxiaodong <990094523@qq.com>
2020-06-17 09:47:07 +0800

    updated

--- src/utils/request.js
+++ src/utils/request.js
@@ -8,29 +8,29 @@ const CODE_SUCCESS = '200'
  * 简易封装网络请求
  *
  */
-// export default async function fetch(options) {
-//   const {url, payload, method = 'GET', showToast = true, autoLogin = true} = options;
-//   const header = {access_token: '这是token'};
-//   if (method === 'POST') {
-//     header['content-type'] = 'application/json';
-//   }
+export default async function fetch(options) {
+  const {url, payload, method = 'GET', showToast = true, autoLogin = true} = options;
+  const header = {access_token: '这是token'};
+  if (method === 'POST') {
+    header['content-type'] = 'application/json';
+  }
 
-//   try {
-//     const res = await Taro.request({url, method, data: payload, header});
-//     const {code, data} = res.data;
-//     if (code !== CODE_SUCCESS) {
-//       return Promise.reject(res.data);
-//     }
-//     return data;
-//   } catch (err) {
-//     if (showToast) {
-//       Taro.showToast({
-//         title: err && err.errorMsg,
-//         icon: 'none',
-//       });
-//     }
+  try {
+    const res = await Taro.request({url, method, data: payload, header});
+    const {code, data} = res.data;
+    if (code !== CODE_SUCCESS) {
+      return Promise.reject(res.data);
+    }
+    return data;
+  } catch (err) {
+    if (showToast) {
+      Taro.showToast({
+        title: err && err.errorMsg,
+        icon: 'none',
+      });
+    }
 
 
 
-//   }
-// }
+  }
+}
